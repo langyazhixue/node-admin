d01b59ea5cf600b600f929b5c9358e6480c33b5d
jiaxiaoxiao <jiaxiaoxiao@idcos.com>
2019-04-16 21:46:41 +0800

    update

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,10 @@
 module.exports = {
   env:'development',
-  port:3008
+  port:3008,
+  database:{
+    host     : '47.97.117.134',
+    user     : 'root',
+    password : '198312jxx',
+    database : 'react'
+  }
 }
