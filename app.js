88acb08d945c488c767c7fa288308bd46d4f6210
jiaxiaoxiao <jiaxiaoxiao@zhexinit.com>
2019-04-16 16:30:41 +0800

    init

---
+++ app.js
@@ -0,0 +1,52 @@
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+// 格式化json
+const json = require('koa-json')
+
+const onerror = require('koa-onerror')
+
+// 解析前端请求过来的参数，并让其成为对象
+const bodyparser = require('koa-bodyparser')
+// 日志打印
+const logger = require('koa-logger')
+
+const index = require('./routes/index')
+const users = require('./routes/users')
+
+// error handler
+onerror(app)
+
+// middlewares
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+
+app.use(json())
+app.use(logger())
+// 静态文件指定
+app.use(require('koa-static')(__dirname + '/public'))
+
+// 配置模版引擎
+app.use(views(__dirname + '/views', {
+  extension: 'pug'
+}))
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
+
+// routes
+app.use(index.routes(), index.allowedMethods()) //当后续所有中间件执行完成之后,判断ctx的status,如果next中间件已经正确处理了response响应,则直接略过
+app.use(users.routes(), users.allowedMethods()) //当后续所有中间件执行完成之后,判断ctx的status,如果next中间件已经正确处理了response响应,则直接略过
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+});
+
+module.exports = app
---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const router = require('koa-router')()
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!'
+  })
+})
+
+router.get('/string', async (ctx, next) => {
+  ctx.body = 'koa2 string'
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
+
+module.exports = router
+
+
+// nodemon 在启动服务之后，修改文件可以自动重启服务
